ca79728c5afccccfb6411653fd6c35f79cad9b58
Easy Learn Java <easylearnjava4u@gmail.com>
2017-10-12 16:08:33 -0500

    Update H2DataSource.java

--- src/main/java/com/easylearnjava/services/H2DataSource.java
+++ src/main/java/com/easylearnjava/services/H2DataSource.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
-import org.h2.tools.Server;
+//import org.h2.tools.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
